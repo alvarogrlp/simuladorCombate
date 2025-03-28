035bf3b678da7ec2483c61d4a70b73516916278c
alvarogrlp <alvarogarciaprof@gmail.com>
2025-03-28 20:29:52 +0000

    cambios de la pantalla de incio

--- src/main/java/es/alvarogrlp/marvelsimu/PrincipalApplication.java
+++ src/main/java/es/alvarogrlp/marvelsimu/PrincipalApplication.java
@@ -3,6 +3,7 @@ package es.alvarogrlp.marvelsimu;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ public class PrincipalApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PrincipalApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 820, 640);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 800);
         stage.setTitle("Pantalla Princial");
         stage.setScene(scene);
         stage.show();
